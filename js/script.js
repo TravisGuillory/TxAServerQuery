0efef9a99b5f43a9de3a2e99eaf9728e06042971
Travis Guillory <travisguillory70@gmail.com>
2022-12-12 00:08:08 +0000

    Updated baseUrl from heroku to github hosting

--- js/script.js
+++ js/script.js
@@ -27,8 +27,8 @@
     });    
 
     async function GetServerInfo(ip, port){
-        let baseUrl = "https://txa-css-status.herokuapp.com/";
-        
+        // let baseUrl = "https://txa-css-status.herokuapp.com/";
+        let baseUrl = "https://travisguillory-bug-free-couscous-755xwv9qr55cp6qv-8000.preview.app.github.dev/";
         console.log(baseUrl + ip + port)
         await $.ajax(baseUrl + ip + port)
         .then(data => {
